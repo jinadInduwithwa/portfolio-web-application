1d6dd697cafe7fec546ffd23889d5547c2a589ed
JINAD INDUWITHWA A.G <140488745+jinadInduwithwa@users.noreply.github.com>
2024-11-16 12:44:12 +0530

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
 
 export default function App() {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:text-yellow-500'>
+      <div className='flixed top-0 -z-10 h-full w-full'></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      
+
+      <div className='container mx-auto px-8'><Navbar/></div>
+     
+    </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../assets/logo only-min.png'
+import { FaDiscord } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <nav className='bg-transparent mb-10 flex item-center justify-between py-2'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img src={logo} alt="logo" className='mx-2 w-12'/>
+      </div>
+      <div className='m-4 flex items-center justify-center text-xl gap-4'>
+        <p className='text-white'>Join with </p>
+        <a href="https://discord.gg/kjGPYhywSA" target='blank'><FaDiscord className='text-3xl text-white'/></a>
+      </div>
+
+    </nav>
+  )
+}
