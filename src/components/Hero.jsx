955fb03ef972cf98fe79a35b2ba764ff45859872
JINAD INDUWITHWA A.G <140488745+jinadInduwithwa@users.noreply.github.com>
2024-11-19 08:07:22 +0530

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants/profile-constants';
 import logo from '../assets/logo with name.jpg';
+import { motion } from 'framer-motion';
+
+const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.5, delay: delay },
+    },
+});
 
 export default function Hero() {
   return (
@@ -9,19 +19,37 @@ export default function Hero() {
         {/* Text Section */}
         <div className='w-full lg:w-1/2 px-4'>
           <div className='flex flex-col items-center lg:items-start'>
-            <h1 className='text-5xl font-light tracking-tighter lg:text-7xl'>Jinad Induvidwa</h1>
-            <span className='mt-4 bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text text-2xl font-medium tracking-tight text-transparent'>
+            <motion.h1
+                variants={container(0)}
+                initial="hidden"
+                animate="visible"
+                className='text-5xl font-light tracking-tighter lg:text-7xl'
+            >
+              Jinad Induvidwa
+            </motion.h1>
+            <motion.span 
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible"
+            className='mt-4 bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text text-2xl font-medium tracking-tight text-transparent'>
               Full Stack Developer | Graphic Designer
-            </span>
-            <p className='my-6 max-w-lg text-center font-light tracking-tighter lg:text-left'>
+            </motion.span>
+            <motion.p 
+            variants={container(1)}
+            initial="hidden"
+            animate="visible"
+            className='my-6 max-w-lg text-center font-light tracking-tighter lg:text-left'>
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
 
         {/* Image Section */}
         <div className='w-full lg:w-1/2 flex justify-center lg:justify-end px-4'>
-          <img 
+          <motion.img 
+          initial={{x:100, opacity:0}}
+          animate={{x:0, opacity:1}}
+          transition={{duration:1, delay:1.2}}
             src={logo} 
             alt="profile" 
             className='max-w-xs lg:max-w-md rounded-md shadow-lg'
--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -6,33 +6,81 @@ import { SiMongodb } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
 import { FaPhp } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
+import { animate, motion } from 'framer-motion';
+
+const iconVariants = (duration) => ({
+    initial:{ y:-10},
+    animate: {
+        y: [10, -10],
+        transition:{
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+})
 
 export default function technologies() {
   return (
     <div className='border-b border-neutral-800 pb-24'>
         <h1 className='my-20 text-center text-4xl'>Technologies</h1>
         <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className='rounded-xl border-4 border-neutral-800 p4'>
                 <FaReact className='text-7xl p-2 text-cyan-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate" className='rounded-xl border-4 border-neutral-800 p4'>
                 <IoLogoNodejs className='text-7xl p-2 text-green-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+             className='rounded-xl border-4 border-neutral-800 p4'>
                 <FaJava className='text-7xl p-2 text-red-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+            <motion.div 
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+             className='rounded-xl border-4 border-neutral-800 p4'>
                 <FaPython className='text-7xl p-2 text-yellow-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+             className='rounded-xl border-4 border-neutral-800 p4'>
                 <FaPhp className='text-7xl p-2 text-blue-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+            <motion.div 
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+             className='rounded-xl border-4 border-neutral-800 p4'>
                 <SiMongodb className='text-7xl p-2 text-green-400'/>
-            </div>
-            <div className='rounded-xl border-4 border-neutral-800 p4'>
+            </motion.div >
+
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+             className='rounded-xl border-4 border-neutral-800 p4'>
                 <SiMysql className='text-7xl p-2 text-blue-400'/>
-            </div>
+            </motion.div >
         </div>
 
     </div>
